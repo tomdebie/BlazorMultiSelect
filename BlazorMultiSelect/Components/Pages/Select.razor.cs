ba84a6f7469c67813588a0c45d751b215c3e0874
Tom De Bie <tompykong@gmail.com>
2025-10-20 00:14:34 +0200

    another

---
+++ BlazorMultiSelect/Components/Pages/Select.razor.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorMultiSelect.Components.Pages;
+
+// public class Select : PageModel
+// {
+//     public void OnGet()
+//     {
+//         
+//     }
+// }
